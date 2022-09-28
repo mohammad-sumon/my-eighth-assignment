b7e55a89f2f87061e16013ed0a6104da62489795
mohammad-sumon <sumon3011@gmail.com>
2022-09-28 23:49:32 +0600

    card degisn complete

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import SingleActivity from '../SingleActivity/SingleActivity';
+import './Activity.css';
 
 const Activity = () => {
     const [activity, setActivity] = useState([]);
@@ -14,8 +15,7 @@ const Activity = () => {
     }, [])
 
     return (
-        <div>
-            <h2>activity here</h2>
+        <div className='activity-container'>
             {
                 activity.map(pd => <SingleActivity 
                     key={pd.id}
